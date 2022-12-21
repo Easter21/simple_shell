61dac38acad3d139a6968b2b7dc0b6873b90bf14
Easter21 <fasika.hailu21@gmail.com>
2022-12-21 00:05:38 -0800

    checking the code

--- checkers.c
+++ checkers.c
@@ -1,18 +1,74 @@
 #include "shell.h"
+
 /**
- *checker- checks to see weather its a built in function
- *@cmd: tokenized user input
- *@buf: line drived fromgetline function
- *Return: 1 if cmd excuted 0 if cmd is not executed
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int checker(char **cmd, char *buf)
+int interactive(info_t *info)
 {
-	if (handle_builtin(cmd, buf))
-		return (1);
-	else if (**cmd == '/')
-	{
-		execution(cmd[0], cmd);
-		return (1);
-	}
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
 	return (0);
+}
+
+/**
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
